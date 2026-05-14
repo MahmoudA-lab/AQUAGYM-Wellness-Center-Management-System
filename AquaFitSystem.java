fea45c6a8ff8ccd8a4e7ad034172a83bac75a0de
ENG. Mahmoud Jabali <kingziad924@gmail.com>
2026-05-14 18:26:26 +0300

    Delete AquaFitSystem.java

--- AquaFitSystem.java
+++
@@ -1,9 +0,0 @@
-package aquafitsystem;
-
-import javax.swing.SwingUtilities;
-
-public class AquaFitSystem {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new MainDashboardFrame("admin").setVisible(true));
-    }
-}
