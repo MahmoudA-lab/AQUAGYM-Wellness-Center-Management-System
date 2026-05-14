6ef5368d9c7d54d1b72de305bf16ae17d15734be
ENG. Mahmoud Jabali <kingziad924@gmail.com>
2026-05-14 18:36:41 +0300

    Add files via upload

---
+++ aquafit/db/DBConnection.java
@@ -0,0 +1,24 @@
+package aquafit.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+   private static final String URL =
+        "jdbc:mysql://164.92.253.36:3306/gym_management_system_database"
+        + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+
+private static final String USER = "12323499_project_db";
+private static final String PASSWORD = "18118915";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Database connection failed.");
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+}
---
+++ aquafit/db/TestConnection.java
@@ -0,0 +1,17 @@
+package aquafit.db;
+
+import java.sql.Connection;
+
+public class TestConnection {
+
+    public static void main(String[] args) {
+
+        Connection con = DBConnection.getConnection();
+
+        if (con != null) {
+            System.out.println("Database connected successfully ✅");
+        } else {
+            System.out.println("Database connection failed ❌");
+        }
+    }
+}
